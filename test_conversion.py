550485034746913d248fa51f28319c8843f23cca
LSibbald <lisettesibbald97@hotmail.com>
2023-06-22 10:05:52 +0200

    add more assert statements to c_to_f function

--- test_conversion.py
+++ test_conversion.py
@@ -2,3 +2,5 @@ from conversion import celsius_to_fahrenheit
 
 def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(20) == 68
+    assert celsius_to_fahrenheit(0) == 32
+    assert celsius_to_fahrenheit(-5) ==23
